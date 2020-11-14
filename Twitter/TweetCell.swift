0eb4547f21a7f2104deb5ec7ce1733c90f0c7837
armankabir <47779642+armankabir@users.noreply.github.com>
2020-11-14 00:23:10 -0500

    Add ability to compose a Tweet, Retweet and Like a Tweet

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -11,8 +11,13 @@ import UIKit
 class TweetCell: UITableViewCell {
 
     @IBOutlet weak var profilePicView: UIImageView! //profile pic for tweeter of tweet
-    @IBOutlet weak var usernameLabel: UILabel!      //username for the tweeter
+    @IBOutlet weak var nameLabel: UILabel!      //username for the tweeter
     @IBOutlet weak var tweetLabel: UILabel!         //content of the tweet itself
+    @IBOutlet weak var twitterHandleLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    var liked: Bool = false
+    var tweetId:Int = -1
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,4 +30,50 @@ class TweetCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    @IBAction func onFavButton(_ sender: Any) {
+        let toBeLiked = !liked
+        if(toBeLiked){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavoriteImage(true)
+            }, failure: { (error) in
+                print("Failed to like. Error code: \(error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                self.setFavoriteImage(false)
+            }, failure: { (error) in
+                print("Failed to unlike. Error code: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func onRetweetButton(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error retweeting! Error Code: \(error)")
+        })
+    }
+    
+    
+    func setFavoriteImage(_ isLiked: Bool){
+        liked = isLiked
+        if(liked){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    func setRetweeted(_ isRetweeted: Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
 }
---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Arman Kabir on 11/13/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onPublishButton(_ sender: Any) {
+        if(!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTWeet(tweetString: tweetTextView.text,
+            success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting this terrt. Error code: \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+        else{
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
